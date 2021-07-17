793bfe4c839f0bfaf75972933af482701b2d26e8
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2021-07-17 23:42:46 +0200

    Form done 90%

--- modal.js
+++ modal.js
@@ -27,7 +27,6 @@ function editNav() {
   function closeModal() {
     modalbg.style.display = "none";
   };
-  modalCross[0].addEventListener ("click", closeModal);
   
   
   
@@ -36,17 +35,9 @@ function editNav() {
   const lastName = document.getElementById ('last');
   const eMail = document.getElementById ('email');
   const birthDate = document.getElementById ('birthdate');
-  const quantityTournament = document.getElementById ('quantity');
   
-  const loc1 = document.getElementById ('location1');
-  const loc2 = document.getElementById ('location2');
-  const loc3 = document.getElementById ('location3');
-  const loc4 = document.getElementById ('location4');
-  const loc5 = document.getElementById ('location5');
-  const loc6 = document.getElementById ('location6');
-  const dateFormat = /^\d{2}[./-]\d{2}[./-]\d{4}$/;
   const numbers = /^[0-9]+$/;
-  const validation = document.getElementById ('checkbox1')
+
   const errorFirst = document.getElementById ('error-first');
   const errorLast = document.getElementById ('error-last');
   const errorMail = document.getElementById ('error-mail');
@@ -69,9 +60,6 @@ function editNav() {
     let lastChecked = false;
     let mailChecked = false;
     let birthChecked = false;
-    let tournamentChecked = false;
-    let radioChecked = false;
-    let conditionsChecked = false;
   
     const parentFirstName = firstName.parentNode;
     if (!firstName.value.match(/(.*[a-z]){2}/i) || firstName.value === ' ' || firstName.value === null || firstName.value.length < 2) {
@@ -101,47 +89,15 @@ function editNav() {
     };
   
     const parent = birthDate.parentNode;
-    const now = new Date();
-    const myBirthDate = new Date(birthDate.value);
-    const parseBirthDate = Date.parse(birthDate.value);
-    if (now < myBirthDate || !parseBirthDate || myBirthDate ===  isNaN){
+    if (!lastName.value.match(/(.*[a-z]){2}/i) || lastName.value === ' ' || lastName.value === null || lastName.value.length < 40){
       parent.setAttribute("data-error", 'Please fill in your date of birth');
       parent.setAttribute("data-error-visible", "true");
       } else {
       parent.setAttribute("data-error-visible", "false");
       birthChecked = true;      
-      }
+      }  
   
-    const parent_1 = quantityTournament.parentNode;
-    if (!quantityTournament.value.match(numbers)) { 
-      parent_1.setAttribute("data-error", 'You must enter a valid number');
-      parent_1.setAttribute("data-error-visible", "true");;
-    } else {
-      parent_1.setAttribute("data-error-visible", "false");
-      tournamentChecked = true;
-    };
-  
-    const parent_2 = loc1.parentNode;
-    if (!loc1.checked && !loc2.checked && !loc3.checked && !loc4.checked && !loc5.checked && !loc6.checked) { 
-      parent_2.setAttribute("data-error", 'A city must be chosen');
-      parent_2.setAttribute("data-error-visible", "true");;          
-    } else {
-      parent_2.setAttribute("data-error-visible", "false");
-      radioChecked = true;
-    };
-  
-  
-    const parent_3 = validation.parentNode;
-    if (!validation.checked) {
-      parent_3.setAttribute("data-error", 'Terms and condition must be accepted');
-      parent_3.setAttribute("data-error-visible", "true");;      
-    } else {
-      parent_3.setAttribute("data-error-visible", "false");
-      conditionsChecked = true;
-    };
-  
-  
-    if (firstChecked && lastChecked && mailChecked & tournamentChecked && radioChecked && conditionsChecked && birthChecked) {
+    if (firstChecked && lastChecked && mailChecked && birthChecked) {
       form.style.display = "none";
       confirmation.style.display = "flex";
       form.reset()
